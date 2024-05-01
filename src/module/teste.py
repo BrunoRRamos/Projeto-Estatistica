684b12c6c98f66d3f46a24ce928572c14622b893
Bruno <bruno.ramos@ccc.ufcg.edu.br>
2024-05-01 19:37:17 -0300

    feat: Modelo e plot

--- src/module/teste.py
+++ src/module/teste.py
@@ -1,7 +1,6 @@
 import pandas as pd
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
+import statsmodels.api as sm
+import matplotlib.pylab as plt
 
 # Carregar os dados
 df = pd.read_csv('../data/College-db.csv', sep=';')
@@ -10,24 +9,30 @@ df = pd.read_csv('../data/College-db.csv', sep=';')
 if "Private" in df.columns:
     df = df.drop(columns=["Private"])
 
+if "Dummy" in df.columns:
+    df = df.drop(columns=["Dummy"])
+
+# Substituindo as "," por "." na coluna "perc.alumni"
+df['perc.alumni'] = df['perc.alumni'].str.replace(",", ".").astype(float)
+
+# Substituindo "Yes", "No" por 1 e 0 na coluna "Apps"
+df['Apps'] = df['Apps'].replace('Yes', 1)
+df['Apps'] = df['Apps'].replace('No', 0)
+
+# Correlação entre as variáveis
+print(df.corr())
+
 # Separar variáveis preditoras (X) e variável resposta (y)
-X = df.drop(columns=["Apps"])
-print(X)
-y = df['Apps'].map({'Yes': 1, 'No': 0})
+X = sm.add_constant(df.drop(columns=["Apps"]))
+y = df['Apps']
 
-# Dividir o conjunto de dados em conjunto de treinamento e conjunto de teste
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+model = sm.OLS(y, X)
+result = model.fit()
 
-# Inicializar o modelo de regressão linear
-model = LinearRegression()
+print(result.summary())
 
-# Treinar o modelo
-model.fit(X_train, y_train)
-
-# Fazer previsões com o conjunto de teste
-y_pred = model.predict(X_test)
-
-# Calcular o erro quadrático médio (MSE) para avaliar o modelo
-mse = mean_squared_error(y_test, y_pred)
-print("Erro Quadrático Médio (MSE):", mse)
-print(df.describe())
+plt.scatter(y, result.fittedvalues)
+plt.xlabel('Valores Reais')
+plt.ylabel('Valores Previstos')
+plt.title('Gráfico de Dispersão: Valores Reais vs. Valores Previstos')
+plt.show()
